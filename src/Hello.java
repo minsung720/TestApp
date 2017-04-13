101c6c8b3106b0bd87ca21ddcae086c05f72db81
kodica0309 <kodica0309@kodica0309-PC>
2017-04-13 10:21:08 +0900

    good bye 문자열 추가

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("ì•ˆë…•");
+		System.out.println("¾È³ç");
+		System.out.println("good bye");
 
 	}
 
